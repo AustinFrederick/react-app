6060fd8e936fe556a66bb9bccb5fa9528578ced1
AustinFrederick <69204799+AustinFrederick@users.noreply.github.com>
2025-07-04 17:33:09 -0600

    resume page now has white text

--- react-app/src/Resume.js
+++ react-app/src/Resume.js
@@ -4,7 +4,7 @@ import ReactiveBackground from "./components/ReactiveBackground";
 export default function Resume() {
     return (
         <ReactiveBackground>
-            <div style={{ maxWidth: 900, margin: "2rem auto", padding: "0 1rem" }}>
+            <div style={{ maxWidth: 900, margin: "2rem auto", padding: "0 1rem", color:"white" }}>
                 <h1>Resume</h1>
                 <p>Link or embed your resume here.</p>
             </div>
